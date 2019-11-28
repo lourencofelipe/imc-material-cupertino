94ea97a4990ae2d03001cddd7a27487e146da776
Felipe <felipe.pucsc@gmail.com>
2019-11-28 20:33:21 -0300

    Adding system verification with Platform

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
+import 'dart:io' show Platform;
 import 'package:flutter/material.dart';
 import 'package:imc_calculator/ui/android/material-app.dart';
 import 'package:imc_calculator/ui/ios/cupertino-app.dart';
 
-void main() => runApp(MyMaterialApp());
-//void main() => runApp(MyCupertinoApp());
+void main() =>
+    Platform.isIOS ? runApp(MyCupertinoApp()) : runApp(MyMaterialApp());
